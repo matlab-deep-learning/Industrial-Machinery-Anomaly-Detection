8d2a827647c63f3b521b6229aad99ab0d7fc4812
Rachel Johnson <rachelj@mathworks.com>
2021-02-23 16:11:57 -0500

    Renamed normal/abnormal to after/before

--- HelperFunctions/plotVibrationData.m
+++ HelperFunctions/plotVibrationData.m
@@ -1,9 +1,9 @@
-function plotVibrationData(normalData, abnormalData, ensMember)
+function plotVibrationData(afterMaintenance, beforeMaintenance, ensMember)
 %PLOTVIBRATIONDATA Visualize all three channels of vibration data
 
 arguments
-    normalData table
-    abnormalData table
+    afterMaintenance table
+    beforeMaintenance table
     ensMember(1,1) double {mustBeInRange(ensMember,1,20,"inclusive")} = 1
 end
 
@@ -11,31 +11,31 @@ figure("Color", "W")
 tiledlayout(3,1)
 
 nexttile()
-plot(1:length(normalData.ch1{ensMember}),normalData.ch1{ensMember})
+plot(1:length(beforeMaintenance.ch1{ensMember}),beforeMaintenance.ch1{ensMember})
 hold on
-plot(1:length(abnormalData.ch1{ensMember}),abnormalData.ch1{ensMember})
+plot(1:length(afterMaintenance.ch1{ensMember}),afterMaintenance.ch1{ensMember})
 hold off
-legend(["Normal Data", "Abnormal Data"])
+legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
 xlabel("Observations")
 title("Channel 1")
 
 nexttile()
-plot(1:length(normalData.ch2{ensMember}),normalData.ch2{ensMember})
+plot(1:length(beforeMaintenance.ch2{ensMember}),beforeMaintenance.ch2{ensMember})
 hold on
-plot(1:length(abnormalData.ch2{ensMember}),abnormalData.ch2{ensMember})
+plot(1:length(afterMaintenance.ch2{ensMember}),afterMaintenance.ch2{ensMember})
 hold off
-legend(["Normal Data", "Abnormal Data"])
+legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
 xlabel("Observations")
 title("Channel 2")
 
 nexttile()
-plot(1:length(normalData.ch3{ensMember}),normalData.ch3{ensMember})
+plot(1:length(beforeMaintenance.ch3{ensMember}),beforeMaintenance.ch3{ensMember})
 hold on
-plot(1:length(abnormalData.ch3{ensMember}),abnormalData.ch3{ensMember})
+plot(1:length(afterMaintenance.ch3{ensMember}),afterMaintenance.ch3{ensMember})
 hold off
-legend(["Normal Data", "Abnormal Data"])
+legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
 xlabel("Observations")
 title("Channel 3")
