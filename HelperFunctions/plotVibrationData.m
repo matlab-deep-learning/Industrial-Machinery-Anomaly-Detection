48b34dcac7bdb96ef6c8548b7bb3088aca4cf93f
Sudheer Nuggehalli <snuggeha@mathworks.com>
2021-03-05 08:55:38 -0500

    minor refactoring updates to demos

--- HelperFunctions/plotVibrationData.m
+++ HelperFunctions/plotVibrationData.m
@@ -1,19 +1,22 @@
-function plotVibrationData(afterMaintenance, beforeMaintenance, ensMember)
+function plotVibrationData(trainData, ensMember)
 %PLOTVIBRATIONDATA Visualize all three channels of vibration data
 
 arguments
-    afterMaintenance table
-    beforeMaintenance table
+    trainData table
     ensMember(1,1) double {mustBeInRange(ensMember,1,20,"inclusive")} = 1
 end
+
+tF = trainData.label == 1;
+afterMaintenance = trainData(tF,:);
+beforeMaintenance = trainData(~tF,:);
 
 figure("Color", "W")
 tiledlayout(3,1)
 
 nexttile()
-plot(1:length(beforeMaintenance.ch1{ensMember}),beforeMaintenance.ch1{ensMember})
+plot(1:length(beforeMaintenance.ch1{ensMember}),beforeMaintenance.ch1{ensMember}, 'r')
 hold on
-plot(1:length(afterMaintenance.ch1{ensMember}),afterMaintenance.ch1{ensMember})
+plot(1:length(afterMaintenance.ch1{ensMember}),afterMaintenance.ch1{ensMember}, 'b')
 hold off
 legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
@@ -21,9 +24,9 @@ xlabel("Observations")
 title("Channel 1")
 
 nexttile()
-plot(1:length(beforeMaintenance.ch2{ensMember}),beforeMaintenance.ch2{ensMember})
+plot(1:length(beforeMaintenance.ch2{ensMember}),beforeMaintenance.ch2{ensMember}, 'r')
 hold on
-plot(1:length(afterMaintenance.ch2{ensMember}),afterMaintenance.ch2{ensMember})
+plot(1:length(afterMaintenance.ch2{ensMember}),afterMaintenance.ch2{ensMember}, 'b')
 hold off
 legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
@@ -31,9 +34,9 @@ xlabel("Observations")
 title("Channel 2")
 
 nexttile()
-plot(1:length(beforeMaintenance.ch3{ensMember}),beforeMaintenance.ch3{ensMember})
+plot(1:length(beforeMaintenance.ch3{ensMember}),beforeMaintenance.ch3{ensMember}, 'r')
 hold on
-plot(1:length(afterMaintenance.ch3{ensMember}),afterMaintenance.ch3{ensMember})
+plot(1:length(afterMaintenance.ch3{ensMember}),afterMaintenance.ch3{ensMember}, 'b')
 hold off
 legend(["Before Maintenance", "After Maintenance"])
 ylabel("Vibration Data")
